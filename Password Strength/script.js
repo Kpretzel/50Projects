313feafb37323482304c2eec15cb1e8d1d95e15b
Kevin Pressl <32285005+Kpretzel@users.noreply.github.com>
2023-08-23 15:39:59 -0500

    Password Strength
    Background detects and unblurs in connection with the strength/length of the password. +Added Show Password Function
    #Note to edit: Add symbol, number, case strengths.

---
+++ Password Strength/script.js
@@ -0,0 +1,17 @@
+const background = document.getElementById('background')
+const password = document.getElementById('password')
+
+password.addEventListener('input', (e) => {
+    const input = e.target.value
+    const length = input.length
+    const blurValue = (20 - length * 2)
+    background.style.filter = `blur(${blurValue}px)`
+})
+
+function showPassword() {
+    if (password.type === 'password') {
+        password.type = 'text'
+    } else {
+        password.type = 'password'
+    }
+}
